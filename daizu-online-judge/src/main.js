42b2549310def4379bab18f5538fc0002f43111e
10riridk0 <mi151316@edu.okinawa-ct.ac.jp>
2019-04-19 17:51:14 +0900

    Vuetifyを導入した

--- daizu-online-judge/src/main.js
+++ daizu-online-judge/src/main.js
@@ -1,13 +1,16 @@
 import Vue from 'vue'
+import './plugins/vuetify'
 import App from './App.vue'
 
 // bootstrap
 import BootstrapVue from 'bootstrap-vue'
 
 Vue.use(BootstrapVue)
+Vue.use(Vuetify)
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import Vuetify from 'vuetify/lib';
 
 Vue.config.productionTip = false
 

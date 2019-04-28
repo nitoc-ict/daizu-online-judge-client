37fff8ae96f338f9bb2f2a23e662393e13fe8527
10riridk0 <mi151316@edu.okinawa-ct.ac.jp>
2019-04-28 23:30:33 +0900

    feat: ヘッダーの文字色変更

--- daizu-online-judge/src/components/modules/app-header.vue
+++ daizu-online-judge/src/components/modules/app-header.vue
@@ -3,12 +3,12 @@
     <v-toolbar class="pa-1" absolute color="grey lighten-5" light>
       <v-toolbar-title style="color: #00897B;">daizu Online Judge</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-toolbar-items>
+      <v-toolbar-items style>
         <v-btn flat>
-          <router-link to="/">Home</router-link>
+          <router-link to="/" style="color: #00897B;text-decoration: none;">Home</router-link>
         </v-btn>
         <v-btn flat>
-          <router-link to="/about">About</router-link>
+          <router-link to="/about" style="color: #00897B;text-decoration: none;">About</router-link>
         </v-btn>
         <v-btn flat>Contests</v-btn>
         <v-btn flat>Sign in</v-btn>

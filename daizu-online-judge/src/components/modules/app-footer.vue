d0066c3ca42ec5b2d5a6ad32d1827ceca5c6749f
10riridk0 <mi151316@edu.okinawa-ct.ac.jp>
2019-04-28 23:22:38 +0900

    feat: フッターを追加

---
+++ daizu-online-judge/src/components/modules/app-footer.vue
@@ -0,0 +1,7 @@
+<template>
+  <v-footer height="auto" color="teal darken-1" absolute="true">
+    <v-layout justify-center row wrap>
+      <v-flex primary lighten-2 py-3 text-xs-center white--text xs12>&copy;2019 ICT Committee</v-flex>
+    </v-layout>
+  </v-footer>
+</template>
--- daizu-online-judge/src/components/views/Home.vue
+++ daizu-online-judge/src/components/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
     <Header/>
+    <Footer/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Header from "../modules/app-header";
+import Footer from "../modules/app-footer";
 
 export default {
   name: "home",
   components: {
-    Header
+    Header,
+    Footer
   }
 };
 </script>

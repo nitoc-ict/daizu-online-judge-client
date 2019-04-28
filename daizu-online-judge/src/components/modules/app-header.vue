89ce949e304c53ffd42845297ab4df29387fdeeb
10riridk0 <mi151316@edu.okinawa-ct.ac.jp>
2019-04-28 22:46:09 +0900

    feat: ガバのヘッダーを追加,

---
+++ daizu-online-judge/src/components/modules/app-header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div style="position: relative;">
+    <v-toolbar absolute color="grey lighten-5" light>
+      <v-toolbar-title style="color: #00897B;">daizu Online Judge</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn flat>
+          <router-link to="/">Home</router-link>
+        </v-btn>
+        <v-btn flat>
+          <router-link to="/about">About</router-link>
+        </v-btn>
+        <v-btn flat>Contests</v-btn>
+        <v-btn flat>Sign in</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+  </div>
+</template>

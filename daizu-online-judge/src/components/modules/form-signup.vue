908b99087dc4a1c093501670aa5a7948a911ea50
10riridk0 <mi151316@edu.okinawa-ct.ac.jp>
2019-05-05 21:38:08 +0900

    feat: 見出しを追加, 中央寄せにした

--- daizu-online-judge/src/components/modules/form-signup.vue
+++ daizu-online-judge/src/components/modules/form-signup.vue
@@ -1,57 +1,53 @@
 <template>
   <div class="form-signup">
-    <v-content>
-      <v-container fluid fill-height>
-        <v-layout align-center justify-center>
-          <v-flex xs12 sm8 md4>
-            <v-card class="elevation-5">
-              <v-toolbar dark class="primary">
-                <v-toolbar-title>Sign Up 😎✨</v-toolbar-title>
-              </v-toolbar>
-              <v-spacer></v-spacer>
-              <v-card-text>
-                <v-form>
-                  <v-text-field
-                    prepend-icon="person"
-                    v-model="name"
-                    :rules="nameRules"
-                    label="Your name"
-                    type="text"
-                    required
-                  ></v-text-field>
-                  <v-text-field
-                    prepend-icon="email"
-                    v-model="email"
-                    :rules="emailRules"
-                    label="E-mail"
-                    required
-                  ></v-text-field>
-                  <v-text-field
-                    prepend-icon="lock"
-                    name="password"
-                    label="Password"
-                    id="password"
-                    type="password"
-                    required
-                  ></v-text-field>
-                  <div class="caption">
-                    <router-link
-                      to="/signin"
-                      class="linkcolor--text pl-2"
-                      style="text-decoration: none;"
-                    >すでにアカウントをお持ちの方はこちら</router-link>
-                  </div>
-                </v-form>
-              </v-card-text>
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <DecisionButton buttonText="OK"/>
-              </v-card-actions>
-            </v-card>
-          </v-flex>
-        </v-layout>
-      </v-container>
-    </v-content>
+    <v-layout align-center justify-center>
+      <v-flex xs12 sm8 md4>
+        <v-card class="elevation-5">
+          <v-toolbar dark class="primary">
+            <v-toolbar-title>Sign Up 😎✨</v-toolbar-title>
+          </v-toolbar>
+          <v-spacer></v-spacer>
+          <v-card-text>
+            <v-form>
+              <v-text-field
+                prepend-icon="person"
+                v-model="name"
+                :rules="nameRules"
+                label="Your name"
+                type="text"
+                required
+              ></v-text-field>
+              <v-text-field
+                prepend-icon="email"
+                v-model="email"
+                :rules="emailRules"
+                label="E-mail"
+                required
+              ></v-text-field>
+              <v-text-field
+                prepend-icon="lock"
+                name="password"
+                label="Password"
+                id="password"
+                type="password"
+                required
+              ></v-text-field>
+              <div class="caption">
+                <router-link
+                  to="/signin"
+                  class="linkcolor--text pl-2"
+                  style="text-decoration: none;"
+                >すでにアカウントをお持ちの方はこちら</router-link>
+              </div>
+            </v-form>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <DecisionButton buttonText="OK"/>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
   </div>
 </template>
 
--- daizu-online-judge/src/components/views/SignUp.vue
+++ daizu-online-judge/src/components/views/SignUp.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="sign-up">
     <v-app>
-      <SignUp/>
+      <!-- <SignUp/> -->
+      <Main/>
     </v-app>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import SignUp from "../modules/form-signup";
+// import SignUp from "../modules/form-signup";
+import Main from "../modules/main-signup";
 
 export default {
   name: "sign-up",
   components: {
-    SignUp
+    // SignUp
+    Main
   }
 };
 </script>

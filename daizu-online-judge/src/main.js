6a1194b02a204bb0d643c491cb6b9457a32a1216
mosmosyrc <prfm_love2.mosmos@icloud.com>
2019-05-06 15:07:39 +0900

    feat:axiossをインストールしてインポートした

--- daizu-online-judge/src/main.js
+++ daizu-online-judge/src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
+<<<<<<< 2459dc561897770e25476319da3cada4ffeaeeb1
 import VueMathjax from 'vue-mathjax'
+=======
+import axios from 'axios'
+>>>>>>> feat:axiossをインストールしてインポートした
 
 // bootstrap
 import BootstrapVue from 'bootstrap-vue'
@@ -17,6 +21,18 @@ import router from './router'
 
 Vue.config.productionTip = false
 
+<<<<<<< 2459dc561897770e25476319da3cada4ffeaeeb1
+=======
+Vue.prototype.$axios = axios;
+
+// Vue.use(Vuetify, {
+//   theme: {
+//     primary: colors.teal.darken1,
+//     accent: colors.red.darken1,
+//     textcolor: colors.grey.darken4
+//   }
+// })
+>>>>>>> feat:axiossをインストールしてインポートした
 new Vue({
   router,
   render: h => h(App)

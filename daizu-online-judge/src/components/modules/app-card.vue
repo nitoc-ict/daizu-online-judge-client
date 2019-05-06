26417be8a8cddaca38bbfe38e8f6dd0464835fa9
mosmosyrc <prfm_love2.mosmos@icloud.com>
2019-05-06 21:04:51 +0900

    feat:添APIを叩いて得たデータのうち、一番上のコンテスト情報を表示させられた

--- daizu-online-judge/src/components/modules/app-card.vue
+++ daizu-online-judge/src/components/modules/app-card.vue
@@ -1,23 +1,20 @@
 <template>
   <div id="app-card">
-    <!--
-    <v-card v-for="contest in contests" :key="contest.id" class="py-2">
+    <v-card class="py-2">
       <v-card-title primary-title>
-        <h3 class="headline mb-0">{{contest.title}}</h3>
+        <h3 class="headline mb-0">{{ contests.results[0].title }}</h3>
       </v-card-title>
 
       <v-card-text>
-        <div>開催日時：{{contest.contest_date}}</div>
-        <div>コンテスト時間：{{contest.contest_time}}</div>
-        <div>writer：{{contest.writer}}</div>
+        <div>開催日時：{{ contests.results[0].startDate }}</div>
+        <div>コンテスト時間：{{ contests.results[0].contestTime }}</div>
+        <div>writer：{{ contests.results[0].writer }}</div>
       </v-card-text>
 
       <v-card-actions>
-        <v-btn flat class="accent">参加する！</v-btn>
+        <v-btn flat class="accent">参加登録</v-btn>
       </v-card-actions>
     </v-card>
-  -->
-    {{ contests }}
   </div>
 </template>
 
@@ -26,14 +23,14 @@ export default {
   el: '#app-card',
   data() {
     return {
-      contests: 'hoge'
+      contests: []
     }
   },
   mounted() {
-    alert(1);
     this.$axios.get('http://127.0.0.1:8000/api/1.0/contests/')
-    .then(response => (this.contests = response));
-    alert(this.contests);
+    .then(response => {
+      this.contests = response.data
+    });
   }
 }
 </script>

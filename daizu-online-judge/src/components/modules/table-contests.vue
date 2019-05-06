c8406b2f74f363e717b31bf1b34dbdd4ee88b42d
10riridk0 <mi151316@edu.okinawa-ct.ac.jp>
2019-05-06 15:20:11 +0900

    feat: コンテスト一覧表のガバのみ追加

---
+++ daizu-online-judge/src/components/modules/table-contests.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="table-contests">
+    <!-- table部 -->
+    <v-data-table
+      :headers="headers"
+      :items="contestList"
+      :search="search"
+      :filter="filter"
+      :class="elevation-5"
+      rows-per-page-items
+      rows-per-page-text
+    >
+      <template v-slot:items="props">
+        <td>{{ props.item.name }}</td>
+        <td class="text-xs-left">{{ props.item.date }}</td>
+        <td class="text-xs-left">{{ props.item.time }}</td>
+      </template>
+      <template v-slot:pageText="props">{{ props.pageStop }} / {{ props.itemsLength }}件表示</template>
+    </v-data-table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      headers: [
+        {
+          text: "コンテスト名",
+          align: "left",
+          sortable: false,
+          value: "name"
+        },
+        { text: "開催日", value: "data" },
+        { text: "時間", value: "time" }
+      ],
+      contestList: [
+        {
+          name: "ICT Welcome Contest 2019",
+          date: "2019/10/10",
+          time: "13:00-14:00"
+        },
+        {
+          name: "Hello world",
+          date: "2019/11/12",
+          time: "00:00:-00:00"
+        },
+        {
+          name: "hoge contest",
+          date: "2020/12/10",
+          time: "16:00-18:00"
+        }
+      ]
+    };
+  }
+};
+</script>
--- daizu-online-judge/src/components/views/Contests.vue
+++ daizu-online-judge/src/components/views/Contests.vue
@@ -2,6 +2,7 @@
   <div id="contests">
     <v-app>
       <Header/>
+      <Table/>
       <Footer/>
     </v-app>
     <router-view/>
@@ -12,11 +13,13 @@
 // @ is an alias to /src
 import Header from "../modules/app-header";
 import Footer from "../modules/app-footer";
+import Table from "../modules/table-contests";
 
 export default {
   name: "contests",
   components: {
     Header,
+    Table,
     Footer
   }
 };

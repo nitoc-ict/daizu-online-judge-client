6b4d08197783497735322c9f4e9bd4c434d16c07
mosmosyrc <prfm_love2.mosmos@icloud.com>
2019-05-07 16:06:13 +0900

    feat:いい感じにFirstViewのクソデカ文字を画面ど真ん中に置けない

--- daizu-online-judge/src/components/modules/main-home.vue
+++ daizu-online-judge/src/components/modules/main-home.vue
@@ -1,15 +1,15 @@
 <template>
   <div id="main-home">
-    <v-layout wrap>
+    <v-layout wrap my-5>
       <v-flex xs12 my-5>
         <!--
         <div class="font-weight-bold display-3 teal--text text--darken-1">#ICT Welcome Contest 2019</div>
         <div class="subheading py-2 textcolor--text">ICTウェルカムコンテストを開催しています.</div>
         <div class="subheading py-2 textcolor--text">皆様奮ってご参加くださいませ.</div>
         -->
+    
         <div class="font-weight-bold display-3 text-lg-center teal--text text--darken-1">Welcome to "daizu Online Judge"</div>
         <div class="subheading py-2 textcolor--text">まだ見ぬ世界を, "daizu Online Judge"で.</div>
-        
         <v-layout align-center row>
           <router-link to="/signin" style="text-decoration: none;">
             <EntryButton buttonText="参加する"/>
@@ -22,14 +22,12 @@
             >アカウント登録はこちら</router-link>
           </div>
         </v-layout>
-        <v-layout>
-        </v-layout>
       </v-flex>
     </v-layout>
 
     <v-layout>
       <v-flex xs12 md6>
-        <div class="font-weight-bold display-2 text-lg-center teal--text text--darken-1">開催中のコンテスト</div>
+        <div class="font-weight-bold display-1 text-lg-center teal--text text--darken-1">開催中のコンテスト</div>
         <Card/>
       </v-flex>
     </v-layout>

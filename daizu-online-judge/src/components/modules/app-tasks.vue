f4e4c9233bd87ebf8b59194e9d1cb47062758de3
10riridk0 <mi151316@edu.okinawa-ct.ac.jp>
2019-05-07 15:50:15 +0900

    feat: 問題文表示するところ追加

--- daizu-online-judge/src/components/modules/app-tasks.vue
+++ daizu-online-judge/src/components/modules/app-tasks.vue
@@ -1,9 +1,14 @@
 <template>
   <div id="app-tasks">
-    <div class="display-1 primary--text mt-5 mb-2">This is "{{ taskTitle }}"</div>
+    <!-- 問題タイトル, 時間制限, メモリ制限 -->
+    <div class="display-1 primary--text mt-5 mb-2 font-weight-medium">{{ taskTitle }}</div>
     <div
-      class="subheading grey--text text--darken-1 mb-5"
-    >時間制限: {{ timeLimit }}/メモリ制限: {{ memoryLimit }}</div>
+      class="subheading grey--text text--darken-1 mb-4"
+    >時間制限: {{ timeLimit }} 秒/メモリ制限: {{ memoryLimit }} MB</div>
+    <v-divider/>
+    <!-- 問題文 -->
+    <div class="body-1 textcolor--text">{{ taskSentence }}</div>
+    <!-- 入力例 -->
   </div>
 </template>
 
@@ -12,7 +17,8 @@ export default {
   props: {
     taskTitle: String,
     timeLimit: String,
-    memoryLimit: String
+    memoryLimit: String,
+    taskSentence: String
   }
 };
 </script>
--- daizu-online-judge/src/components/views/Task.vue
+++ daizu-online-judge/src/components/views/Task.vue
@@ -1,20 +1,28 @@
 <template>
   <div id="task">
     <v-app>
+      <Header/>
       <v-content>
-        <Task/>
+        <v-container>
+          <Task taskTitle="A: Hello World!" timeLimit="1" memoryLimit="256" taskSentence="wawa"/>
+        </v-container>
       </v-content>
+      <Footer/>
     </v-app>
     <router-view/>
   </div>
 </template>
 
 <script>
+import Header from "../modules/app-header";
+import Footer from "../modules/app-footer";
 import Task from "../modules/app-tasks";
 export default {
   name: "task",
   components: {
-    Task
+    Header,
+    Task,
+    Footer
   }
 };
 </script>

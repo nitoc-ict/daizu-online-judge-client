0861b989a592ebbeae6462055753cfa585755171
10riridk0 <mi151316@edu.okinawa-ct.ac.jp>
2019-05-08 23:24:48 +0900

    feat: 提出ボタンを押したらポップアップが出るようになった

--- daizu-online-judge/src/components/modules/main-submit.vue
+++ daizu-online-judge/src/components/modules/main-submit.vue
@@ -12,16 +12,35 @@
         style="font-family: Consolas, 'Courier New', Courier, Monaco, monospace;"
       ></v-textarea>
     </v-layout>
-    <SubmitButton buttonText="提出"/>
+    <!-- 提出ボタン -->
+    <v-btn dark color="accent" @click.stop="dialog=true">提出</v-btn>
+    <v-dialog v-model="dialog" max-width="320">
+      <!-- ダイアログボックス -->
+      <v-card>
+        <v-card-title class="headline primary--text">提出完了</v-card-title>
+        <v-card-text class="body-1 textcolor--text">提出状況タブで結果を確認してみましょう！</v-card-text>
+        <v-card-actions>
+          <v-spacer/>
+          <v-btn color="primary" @click="dialog=false">OK</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 <script>
 import Selectbox from "./app-selectbox";
-import SubmitButton from "./app-button";
+// import SubmitButton from "./app-button";
+// import Dialog from "./app-dialog";
 export default {
   components: {
-    Selectbox,
-    SubmitButton
+    Selectbox
+    // SubmitButton
+    // Dialog
+  },
+  data() {
+    return {
+      dialog: false
+    };
   }
 };
 </script>

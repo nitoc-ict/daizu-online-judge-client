581b1953378976bbcc47c863c5cd13a60d419cc6
10riridk0 <mi151316@edu.okinawa-ct.ac.jp>
2019-05-08 14:34:01 +0900

    feat: headerを固定した

--- daizu-online-judge/src/components/modules/app-header.vue
+++ daizu-online-judge/src/components/modules/app-header.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-toolbar class="pa-1" light>
+  <v-toolbar class="pa-1" light fixed>
     <v-toolbar-title class="primary--text">
       <router-link to="/" id="logo-daizu" style="text-decoration: none;">daizu Online Judge</router-link>
     </v-toolbar-title>
